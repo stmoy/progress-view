80d3d587cdde1e203f5121ef95fcada79d4c5f1a
Steven Moyes <stmoy@microsoft.com>
2020-04-01 13:37:57 -0700

    Add windows solution

---
+++ windows/progress-view-windows/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progress_view_windows
+{
+    public sealed class Class1
+    {
+    }
+}
