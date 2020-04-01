b7d2ba52b45261e9110a55200ab6a6509c38e347
Steven Moyes <stmoy@microsoft.com>
2020-04-01 14:53:54 -0700

    Add custom view and view manager

---
+++ windows/progress-view-windows/ProgressViewControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace progress_view_windows
+{
+    public sealed partial class ProgressViewControl : UserControl
+    {
+        public ProgressViewControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ windows/progress-view-windows/ProgressViewControlViewManager.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Controls;
+
+using Microsoft.ReactNative.Managed;
+using System.Collections.Generic;
+
+namespace progress_view_windows
+{
+    class ProgressViewControlViewManager : AttributedViewManager<ProgressViewControl>
+    {
+
+    }
+}
